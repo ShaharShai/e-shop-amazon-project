f0c439865916136fa1c8c9d72a1d0adbe059e3b2
Shahar <shahars839@gmail.com>
2024-01-16 14:58:35 +0200

    changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import User from './models/User.js';
 
 const app = express();
 
@@ -8,7 +9,11 @@ app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({extended: false}));
 
-// app.post('/addUser', routes);
+app.post('/adduser', async (req, res) => {
+    const user = req.body;
+    const result = await User.create(user);
+    res.send(result);
+});
 
 mongoose.connect("mongodb+srv://shahar:12345@e-shop.r1l6bex.mongodb.net/e-shop-amazon-project?retryWrites=true&w=majority")
 .then(() => app.listen(5000, () => console.log(`Listening on ${5000}`)))
