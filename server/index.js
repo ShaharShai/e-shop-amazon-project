98389c9f427345944089f4a49cbc8d2c5971e801
Shahar <shahars839@gmail.com>
2024-01-16 14:43:31 +0200

    add mongoose connect

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -7,4 +8,8 @@ app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({extended: false}));
 
-app.listen(5000, () => console.log(`Listening on ${8080}`));
+// app.post('/addUser', routes);
+
+mongoose.connect("mongodb+srv://shahar:12345@e-shop.r1l6bex.mongodb.net/e-shop-amazon-project?retryWrites=true&w=majority")
+.then(() => app.listen(5000, () => console.log(`Listening on ${5000}`)))
+.catch(err => console.error(err));
